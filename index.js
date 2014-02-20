6d05ffabe39241bb38535e385a0e92cb63318f4d
SuzhouKada <open@suzhoukada.com>
2014-02-20 14:58:30 +0800

    Add page2images-visual-link

---
+++ index.js
@@ -0,0 +1,2 @@
+var p2iQurey = require('./p2i.thumbnails.js');
+p2iQurey().run();
---
+++ p2i.thumbnails.js
@@ -0,0 +1,25 @@
+/***
+ *   http://www.page2images.com
+ *
+ *   ************demo start************
+ *   // the first para is apikey, default is free key.
+ *   // the second para is preload, if you want to load images when document load
+ *   // the third para is includeinnerlink, if your screenshots include current domain.
+ *   p2iQuery().run("Free", false, false);
+ *   // Please notice your domain when use YOUR_DIRECTLINK_KEY.
+ *   p2iQuery().run(YOUR_DIRECTLINK_KEY, false, true);
+ *   
+ *   Please add below scripts in your html.
+ *   ===========================================================================
+ *   <script type="text/javascript" src="/js/p2i.thumbnails.js"></script>
+ *   <script>
+ *   	window.onload=function() {
+ *   		p2iQuery().run("6b51f3e77d98e07f", false, true);
+ *   		//p2iQuery().run("Free", true, true);
+ *   	};
+ *   </script>
+ *   ===========================================================================
+ *   ************demo end************
+ ***/
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('(c(){7 2=c(){d 2.T.W()};2.T=2.1p={2:"1.0.0",W:c(){d 6},1B:c(m,E,B){6.m=m||"I";6.E=E||g;6.B=B||g;6.n=r;7 x=m=="I";6.U(x);6.1c()},U:c(x){9(3.p(\'z\')==1C){7 C="<v b=\'J: N R #L;\'><1a  X=\'P\' A=\'\' />";9(x){C+=\'<v b="J-14: N R #L; 1v: 0 1x;  1w-1y: 1t;"><a 8="q://O.G.H" b="1u:#L; 1D-1E:1z;">1A 1s 1k 1l</a></v>\'};C+="</v>";7 4=3.13("v");4.X="z";4.b.1i="#1m";4.b.y="M";4.b.1q="1n";4.b.1o=1r;4.b.J="N R #1Z";4.1U=C;3.o.17(4,3.o.1h[0])}},18:c(8){8=8.1V(/[/]$/,\'\');9(8==3.w.8){d g};7 F=j h("^1W");9(F.k(8)){d g};9(6.B){d r};9((j h("^q[s]?://")).k(8)||(j h("^O")).k(8)){F=j h(3.w.Z);9(F.k(8)){d g}D{d r}}D{d g}},Q:c(e){9(!(j h("^q[s]?://")).k(e)&&!(j h("^O")).k(e)){9((j h("^/")).k(e)){e=3.w.Z+e}D{e=3.w.8+\'/\'+e}};9(6.m==\'I\'){d"q://Y.G.H/20?11=1&10="+e}D{d"q://Y.G.H/1G?1H="+6.m+"&11=1&10="+e}},19:c(4){4.1P(\'1Q\',c(f){2().n=r;2().V=2().Q(6.8);2().f=f;2().1b=1j(c(){2().n=r;9(!2().n){d};7 S=2().V;3.p("P").A=S;7 l=3.p("z");l.b.y=\'1J\';7 f=2().f;7 u=f.1N+15;7 1d=3.o.1e||3.1M.1e;7 12=f.1L+1d+15;9(u+l.16>1f.1O){u=u-l.16-1F};l.b.1K=u+\'1g\';l.b.14=12+\'1g\';2().n=g},1I)});4.1R=c(f){2().n=g;1X(2().1b);3.p("z").b.y=\'M\';3.p("P").A=\'\'}},1c:c(){7 t=3.1Y("a");7 K=0;1T(7 i=0;i<t.1S;i++){9(6.18(t[i].8)){6.19(t[i]);9(6.E&&K<5){K++;7 4=3.13("1a");4.A=6.Q(t[i].8);4.b="y:M;";3.o.17(4,3.o.1h[0])}}}},};1f.2=2})();',62,125,'||p2iQuery|document|elem||this|var|href|if||style|function|return|url|evt|false|RegExp||new|test|p2i_popup|apikey|isonmouseover|body|getElementById|http|true||all_alinks|pleft|div|location|is_free|display|page2images_popup|src|includeinnerlink|p2i_pop|else|preload|p2ireg|page2images|com|Free|border|preload_count|222222|none|1px|www|page2images_img|get_img_api_url|solid|img_src|fn|add_page2images_popup|current_href|init|id|api|hostname|p2i_url|p2i_visual_link|ptop|createElement|top||offsetWidth|insertBefore|verified_href|setupmouseoverout|img|Timer|preload_all_a_link_tags|sp|scrollTop|window|px|childNodes|background|setTimeout|By|Page2Images|FFFFFF|absolute|zIndex|prototype|position|999|Preview|right|color|padding|text|3px|align|10px|Website|run|null|font|size|30|directlink|p2i_key|500|block|left|clientY|documentElement|clientX|innerWidth|addEventListener|mouseover|onmouseout|length|for|innerHTML|replace|javascript|clearTimeout|getElementsByTagName|DDDDDD|freeimagetag'.split('|'),0,{}))
+
